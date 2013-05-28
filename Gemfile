15daecc57ac9673f371e0ad36039bf9f9c91eec6
Michael Dunnegan <mdunnegan@spicy.io>
2013-05-28 16:22:28 -0400

    buncha stuff

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.13'
+gem 'bootstrap-sass', '2.1'
 
 group :development, :test do
   gem 'sqlite3', '1.3.5'
@@ -18,7 +19,7 @@ end
 gem 'jquery-rails', '2.0.2'
 
 group :test do
-  gem 'capybara', '1.1.2'
+  gem 'capybara'
 end
 
 group :production do
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  root to: 'static_pages#home'
 
-  get "static_pages/help"
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contacts', to: 'static_pages#contacts'
 
-  get "static_pages/about"
+  
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
 
-  get "static_pages/contacts"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
